d690ad6e80d49c396c689f33c297f6ae62e57330
Forrest Wolf <Forrest.Weiswolf@gmail.com>
2016-09-08 09:39:33 -0400

    done

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,23 @@
 $(document).ready(function(){
-	for (var i = 0; i <= 16; i++) {
-		$(this).find('body').append('<div class="row"></p>');
-		for (var j = 0; j <= 16; j++) {
-			$(this).find('body').append('<div></div>');
-		}
-	}
+	$('.container').on('mouseenter', '.square', function(){
+		$(this).toggleClass('drawn');
+	});
+	$('#new-grid').on('click', function() {
+		var dim = prompt("Enter grid dimension:")
+		createGrid(dim);
+	});
 });
+
+function createGrid(dim) {
+	var container = $('.container')
+	var squareWidth = container.width()/dim;
+  	var squareHeight = container.height()/dim;
+	container.empty();
+	for (var i = 0; i < dim; i++) {
+		container.append('<div class="row"></div>');
+	}
+	for (var j = 0; j < dim; j++) {
+			container.find('.row').append('<div class="square"></div>');
+	}	
+	$(".square").css({'width':squareWidth, 'height':squareHeight});
+}
